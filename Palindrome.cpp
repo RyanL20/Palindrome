975a1441445e639abaf6c5f82242ac1e162e21ce
RyanL20 <ryannhatkhoa@gmail.com>
2020-10-04 03:52:06 +0100

    Fixed the backwards string array

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdio>
 #include <cstring>
 
 using namespace std;
@@ -6,19 +7,22 @@ using namespace std;
 int main() {
   
   char str[20];
-  char str2[20];
+  char str2[20] = {'\0'};
   cin.get(str, 20);
   cin.get();
   cout << str2 << endl;
   int count = strlen(str) - 1;
-  for (int i = 0; i <= strlen(str); i++) {
+
+  // implement and test reverse string array.
+  for (int i = 0; i <= strlen(str)-1; i++) {
     if (str[i] != '\n') {
+      
       str2[count] = str[i];
 
      
       cout << str2[count] << endl;
       cout << count << endl;
-      cout << str2 << endl;
+      
       if (count != 0) {
 	count--;
       }
@@ -27,7 +31,7 @@ int main() {
     }
    }
   
-  cout << str << endl;
-  cout << str2 << endl;
+   cout << str << endl;
+   cout << str2 << endl;
   return 0;
 }
