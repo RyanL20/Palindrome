c2e79a15c01a316b16d7c3f5f697dcae59b52279
RyanL20 <ryannhatkhoa@gmail.com>
2020-10-04 09:25:17 +0100

    Includes removes space, reverse string array, comparing

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -6,32 +6,53 @@ using namespace std;
 
 int main() {
   
-  char str[20];
-  char str2[20] = {'\0'};
-  cin.get(str, 20);
+  char input_str[81];
+  char nospace_str[81] = {'\0'};
+  char reverse_str[81] = {'\0'};
+
+  cout << "Is it a Palindrome?" << endl;
+  cout << "Enter up to 80 characters to check." << endl;
+  cin.get(input_str, 80);
   cin.get();
-  cout << str2 << endl;
-  int count = strlen(str) - 1;
-
-  // implement and test reverse string array.
-  for (int i = 0; i <= strlen(str)-1; i++) {
-    if (str[i] != '\n') {
-      
-      str2[count] = str[i];
-
-     
-      cout << str2[count] << endl;
-      cout << count << endl;
-      
-      if (count != 0) {
-	count--;
-      }
-    } else {
-   
-    }
-   }
   
-   cout << str << endl;
-   cout << str2 << endl;
+  int count;
+  int j = 0;
+  
+  //need to remove punctuation and change to lowercase.
+
+  
+  // removes space, copy to nospace_string
+  for (int i = 0; i < strlen(input_str); i++) {
+    if (input_str[i] != '\n') {
+      if (input_str[i] != ' ') {
+	nospace_str[j] = input_str[i];
+	j++;
+      }
+    }
+  }
+  
+  //reverse string array
+  count = strlen(nospace_str) - 1;
+  
+  for (int a = 0; a < strlen(nospace_str); a++) {
+    reverse_str[count] = nospace_str[a]; 
+
+    if (count != 0) {
+      count--;
+    } 
+     
+  }
+  
+  //Compare the string to see if they are equal
+  if (strcmp(nospace_str, reverse_str) == 0) {
+    cout << "It's a Palindrome" << endl;
+  }
+  else {
+    cout << "Not a Palindrome" << endl;   
+  }
+     
+  cout << input_str << endl;
+  cout << nospace_str << endl;
+  cout << reverse_str << endl;
   return 0;
 }
